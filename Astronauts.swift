7169ca46d70566d1bd841038693fd1b50f37da62
Louis Lathrop <ll0128774@otc.edu>
2024-04-24 09:46:41 -0500

    API info

---
+++ Astronauts.swift
@@ -0,0 +1,18 @@
+//
+//  Astronauts.swift
+//  spaceTeam
+//
+//  Created by LATHROP, LOUIS W. on 4/24/24.
+//
+
+import SwiftUI
+
+struct Astronauts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Astronauts()
+}
--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,7 @@
 //
 //  ContentView.swift
 //  spaceTeam
-//  Account Email: ll0128774@otc.edu
-//  Account ID: 06af316e-da75-48cc-bfbd-3042585c3e7c
+//
 //  Created by HARMON, AUDREY on 4/24/24.
 //
 
