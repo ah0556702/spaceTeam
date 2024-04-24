6603b7f1c44496ad987538ee92da173406edfcb9
Audrey Harmon <ah0556702@otc.edu>
2024-04-24 09:26:00 -0500

    Initial Commit

---
+++ spaceTeam/spaceTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceTeamApp.swift
+//  spaceTeam
+//
+//  Created by HARMON, AUDREY on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct spaceTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
